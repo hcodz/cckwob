c94b2a8e24e9d28207ce0b03c16812aa53e406ff
hcodz <147790878+hcodz@users.noreply.github.com>
2023-10-16 21:36:19 +0700

    Update updatelink.php

--- updatelink.php
+++ updatelink.php
@@ -1,2 +1,2 @@
-$url = "https://github.com/hcodz/cckwob/raw/main/GolikeV5.5_enc.php";
+$url = "https://github.com/hcodz/cckwob/raw/main/GolikeV5.5-1.php";
 file_put_contents("updatelink.txt", $url);
